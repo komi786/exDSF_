34f1f3b787b1b1440bb945325a8ca80d181025ff
Komal Gilani <komalsyeda29@gmail.com>
2023-12-15 14:30:57 +0100

    Initial commit

---
+++ dataset_summary.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from generate_summary import *
+# Press the green button in the gutter to run the script.
+import generate_summary
+if __name__ == '__main__':
+    dc = DatasetCreator("/Users/komalgilani/Downloads/patient_register_UMV1.sav")
+    dc.create_output_dataset()
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
