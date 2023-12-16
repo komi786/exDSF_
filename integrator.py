0141bbb5c953d0475d2e48a45db78dfa76b2064e
Komal Gilani <komalsyeda29@gmail.com>
2023-12-16 15:32:39 +0100

    Update integrator.py

--- integrator.py
+++ integrator.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 from generate_summary import *
 # Press the green button in the gutter to run the script.
 import generate_summary
@@ -12,4 +7,3 @@ if __name__ == '__main__':
     dc.create_output_dataset(filepath)
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
