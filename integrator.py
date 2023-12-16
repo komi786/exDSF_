361ad08ceb454ef5c64b59a834fc6e6e0528ad86
Komal Gilani <komalsyeda29@gmail.com>
2023-12-16 15:32:12 +0100

    Update integrator.py

--- integrator.py
+++ integrator.py
@@ -7,7 +7,7 @@ from generate_summary import *
 # Press the green button in the gutter to run the script.
 import generate_summary
 if __name__ == '__main__':
-    filepath = '/Users/komalgilani/Downloads/patient_register_UMV1.sav'
+    filepath = '/Users/user_name/Downloads/patient_register_UMV1.sav'
     dc = DatasetCreator()
     dc.create_output_dataset(filepath)
 
